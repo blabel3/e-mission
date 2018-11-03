c3b7ca8f4c75c3bc1ace019de70eaf78d20d9932
AbdurRahman <abdura99@gmail.com>
2018-11-03 01:06:09 -0400

    Page1
    The first page of the app!

---
+++ app/src/main/java/com/emission/abnc/e_mission/Page1.kt
@@ -0,0 +1,22 @@
+package com.emission.abnc.e_mission
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_page1.*
+
+class Page1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_page1)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
