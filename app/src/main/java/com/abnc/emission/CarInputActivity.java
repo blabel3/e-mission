0ecae477256e705045fdfa4bee87b3df469d4d01
Blake Abel <blakecabel@gmail.com>
2018-11-04 04:09:18 -0500

    Removed toolbar from car selection

--- app/src/main/java/com/abnc/emission/CarInputActivity.java
+++ app/src/main/java/com/abnc/emission/CarInputActivity.java
@@ -39,17 +39,6 @@ public class CarInputActivity extends AppCompatActivity implements AdapterView.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_input);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         cars = new ArrayList<>(0);
 
