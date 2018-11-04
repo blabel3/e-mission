4e8c604a9859988280507d3d22b7a1b8d1b61a2e
Blake Abel <blakecabel@gmail.com>
2018-11-04 01:33:06 -0500

    Car data java

--- app/src/main/java/com/abnc/emission/CarInputActivity.java
+++ app/src/main/java/com/abnc/emission/CarInputActivity.java
@@ -1,11 +1,20 @@
 package com.abnc.emission;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 
 public class CarInputActivity extends AppCompatActivity {
 
@@ -24,6 +33,34 @@ public class CarInputActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ArrayList<Car> cars = new ArrayList<>(0);
+
+        try{
+            InputStream carStream = getAssets().open("carData.json");
+            cars = DataParser.readJSON(carStream);
+        } catch (IOException oops){
+            Log.e("FileError", oops.toString());
+        }
+
+        ArrayList<String> make = new ArrayList<>(0);
+        ArrayList<String> model = new ArrayList<>(0);
+        ArrayList<String> year = new ArrayList<>(0);
+
+        for(Car car : cars){
+            make.add(car.getMake());
+            model.add(car.getModel());
+            year.add(Integer.toString(car.getYear()));
+        }
+
+        ArrayAdapter<String> makeAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, make);
+        ArrayAdapter<String> modelAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, model);
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, year);
+
+        ((Spinner)findViewById(R.id.makeSpinner)).setAdapter(makeAdapter);
+        ((Spinner)findViewById(R.id.modelSpinner)).setAdapter(modelAdapter);
+        ((Spinner)findViewById(R.id.yearSpinner)).setAdapter(yearAdapter);
+
     }
 
 }
