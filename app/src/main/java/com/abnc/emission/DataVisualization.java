47e6b6d88fedf49de7c26b93cb99a6097d869df8
Blake Abel <blakecabel@gmail.com>
2018-11-03 20:22:55 -0400

    Got pagination to work on the main activity

--- app/src/main/java/com/abnc/emission/DataVisualization.java
+++ app/src/main/java/com/abnc/emission/DataVisualization.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
  * Use the {@link DataVisualization#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DataVisualization extends Fragment {
+public class DataVisualization extends androidx.fragment.app.Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,6 +58,7 @@ public class DataVisualization extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
--- app/src/main/java/com/abnc/emission/MainActivity.java
+++ app/src/main/java/com/abnc/emission/MainActivity.java
@@ -1,18 +1,104 @@
 package com.abnc.emission;
 
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.fragment.app.*;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends FragmentActivity implements DataVisualization.OnFragmentInteractionListener, Trip.OnFragmentInteractionListener {
+
+    static final int NUM_ITEMS = 3;
+
+    MyAdapter mAdapter;
+
+    ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        /*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, DataVisualization.newInstance())
-                    .commitNow();
-        }*/
+
+        /*FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        DataVisualization dataVisFragment = new DataVisualization();
+        fragmentTransaction.add(R.id.fragment_holder, dataVisFragment);
+        fragmentTransaction.commit();    FOR CHANGING FRAGMENTS*/
+
+        mAdapter = new MyAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager)findViewById(R.id.fragment_holder);
+        mPager.setAdapter(mAdapter);
+
+        BottomNavigationView navBar = (BottomNavigationView) findViewById(R.id.main_navigation);
+        navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch(menuItem.getItemId()) {
+                    case R.id.savings_option:
+                        mPager.setCurrentItem(0);
+                        return true;
+                    case R.id.trip_option:
+                        mPager.setCurrentItem(1);
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
+
+    }
+
+    public static class MyAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<Fragment> fragments;
+
+        public MyAdapter(FragmentManager fm) {
+            super(fm);
+
+            DataVisualization dataScreen = DataVisualization.newInstance("test", "testt");
+            Trip tripScreen = Trip.newInstance("haha", "herherh");
+
+            //fragments.add(0, dataScreen);
+            //fragments.add(1, tripScreen);
+
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            //return ArrayListFragment.newInstance(position);
+            //return fragments.get(position);
+            if(position ==0) {
+                return new DataVisualization();
+            } else {
+                return new Trip();
+            }
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
